a008f6300cb88e56b7123028df5806f6e06b3031
Blake Embrey <hello@blakeembrey.com>
2017-04-11 14:28:09 -0700

    Improve type overload for when schema is a string

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,11 @@ export type Schema = string | SchemaObject | SchemaArray
 /**
  * Map the input to the output using the schema.
  */
-export function map <T extends object> (schema: SchemaObject, input: any): T | undefined
-export function map <T extends any[]> (schema: SchemaArray, input: any): T | undefined
-export function map <T extends any> (schema: Schema, input: any): T | undefined
-export function map <T extends any> (schema: Schema, input: any): T | undefined {
+export function map <T extends object> (schema: SchemaObject, input: any): T
+export function map <T extends any[]> (schema: SchemaArray, input: any): T
+export function map <T> (schema: string, input: any): T | undefined
+export function map <T> (schema: Schema, input: any): T | undefined
+export function map <T> (schema: Schema, input: any): T | undefined {
   if (typeof schema === 'string') {
     return get<T | undefined>(input, parse(schema))
   }
